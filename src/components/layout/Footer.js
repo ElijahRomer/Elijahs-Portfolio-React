1a5e1361f96f7d169fd451b2aaa5b8b1dae23bab
Elijah Romer <romere88@gmail.com>
2021-11-01 18:13:05 -0500

    added links to GitHub, LinkedIn, and Stack Overflow to the footer. Added custom css module and imported to the Footer for scoped styling. Buttons in footer now stack and change color when hovered. Links also work.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import classes from './Footer.module.css';
 
 function Footer() {
   return (
     <footer className="footer mt-auto py-3 bg-primary">
       <div className="container text-center">
-        <span className="text-light">© 2021 Elijah Romer.</span>
+        <span className="text-light">Follow me at: </span>
+        <br />
+        <a
+          id={classes.footerLink}
+          className="text-light footer-link"
+          href="https://github.com/ElijahRomer"
+        >
+          <i className="fab fa-github"></i> GitHub
+        </a>
+        <br />
+        <a
+          id={classes.footerLink}
+          className="text-light footer-link"
+          href="https://www.linkedin.com/in/ElijahRomer/"
+        >
+          <i className="fab fa-linkedin"></i> LinkedIn
+        </a>
+        <br />
+        <a
+          id={classes.footerLink}
+          className="text-light footer-link"
+          href="https://stackoverflow.com/users/15502947/elijah-romer?tab=profile"
+        >
+          <i className="fab fa-stack-overflow"></i> Stack Overflow
+        </a>
+        <br />
+        <br />
+        <span className="text-light">
+          © 2021 Elijah Romer. All rights reserved.
+        </span>
       </div>
     </footer>
   );
