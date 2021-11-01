3004e010b580c28388f8fa4bf7e9296ce9a94b50
Elijah Romer <romere88@gmail.com>
2021-11-01 15:03:57 -0500

    fixed issue with NavBar not highlighting current page.

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -4,26 +4,28 @@ import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
+import { NavLink } from 'react-router-dom';
+
 function MainNavigation() {
   return (
     <Navbar bg="primary" variant="dark" expand="lg">
       <Container>
-        <LinkContainer to="/">
-          <Navbar.Brand href="/">Elijah Romer</Navbar.Brand>
+        <LinkContainer to="/" exact>
+          <Navbar.Brand>Elijah Romer</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
           <Nav>
-            <LinkContainer to="/">
+            <LinkContainer to="/" exact activeClassName="active">
               <Nav.Link>Bio</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/work">
+            <LinkContainer to="/work" exact activeClassName="active">
               <Nav.Link>Work</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/resume">
+            <LinkContainer to="/resume" exact activeClassName="active">
               <Nav.Link>Resume</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/contact">
+            <LinkContainer to="/contact" exact activeClassName="active">
               <Nav.Link>Contact</Nav.Link>
             </LinkContainer>
           </Nav>
