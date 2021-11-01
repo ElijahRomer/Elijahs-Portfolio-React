2726568fdda0a4cf2651afcc5d126763fe0505d9
Elijah Romer <romere88@gmail.com>
2021-11-01 03:18:23 -0500

    added photos and added beginning content for Bio page. Added import for fo-Awesome icons

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -4,7 +4,15 @@ import Card from '../components/ui/Card';
 function BioPage() {
   return (
     <Card>
-      <h1>This is the BIO Page</h1>
+      <div class="row my-4">
+        <h1 class="text-center">Hello! I'm Elijah Romer.</h1>
+        <div class="row my-2">
+          <button class="btn btn-primary col-sm-10 col-md-4 mx-auto" id="work">
+            View my work <i class="fas fa-arrow-right"></i>
+          </button>
+        </div>
+      </div>
+      <hr />
     </Card>
   );
 }
