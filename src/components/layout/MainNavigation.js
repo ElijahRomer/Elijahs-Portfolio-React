b47c5d63d9f2d4b1b4a4732e8fee4a073e253fbd
Elijah Romer <romere88@gmail.com>
2021-11-02 01:09:42 -0500

    added onClick event handlers to nav links to auto-collapse the drop down when a nav item is clicked.

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,30 +1,34 @@
 // import { Fragment } from 'react';
 // import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 function MainNavigation() {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <Navbar bg="primary" variant="dark" expand="lg">
+    <Navbar bg="primary" variant="dark" expand="lg" expanded={expanded}>
       <Container>
         <LinkContainer to="/" exact>
           <Navbar.Brand>Elijah Romer</Navbar.Brand>
         </LinkContainer>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          onClick={() => setExpanded(expanded ? false : 'expanded')}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
           <Nav>
             <LinkContainer to="/" exact activeClassName="active">
-              <Nav.Link>Bio</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)}>Bio</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/work" exact activeClassName="active">
-              <Nav.Link>Work</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)}>Work</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/resume" exact activeClassName="active">
-              <Nav.Link>Resume</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)}>Resume</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/contact" exact activeClassName="active">
-              <Nav.Link>Contact</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)}>Contact</Nav.Link>
             </LinkContainer>
           </Nav>
         </Navbar.Collapse>
